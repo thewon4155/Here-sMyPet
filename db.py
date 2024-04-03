f911bdaa849dcc0d743ecb51012c61c836f2e0ee
kwonjinwoo <158565724+kwonjinwoo@users.noreply.github.com>
2024-04-03 11:43:46 +0900

    Create db.py

---
+++ db.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db = SQLAlchemy(app)
+
+
+# 회원가입
+
+
+
+
+
+
+
+
+# 로그인 기능
+
+class login (db.Model)
